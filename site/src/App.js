bd1fa15741b22160efe808cacc052fa0dddaa604
Anastasiia Noguier <noguier@usc.edu>
2024-03-02 20:38:09 -0800

    Querying and displaying search results, everything works except amenities

--- site/src/App.js
+++ site/src/App.js
@@ -1,26 +1,27 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Header from "/components/Header"
-import Footer from "/components/Footer"
-import Search from "/pages/Search"
-import Login from "/pages/Login"
-import Favorites from "/pages/Favorites"
-import Compare from "/pages/Compare"
+// import Header from "/components/Header"
+// import Footer from "/components/Footer"
+import Search from "./pages/Search"
+import Login from "./pages/Login"
+import Favorites from "./pages/Favorites"
+import Compare from "./pages/Compare"
 
 //these are all the pages, we can change Compare page to include both features later
 
 function App() {
     return (
         <div>
-            <Header/>
+            {/*<Header/>*/}
             <Routes>
+                <Route path="/" element={<Login />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/search" element={<Search />} />
                 <Route path="/favorites" element={<Favorites />} />
                 <Route path="/compare" element={<Compare />} />
-                <Route path="*" element={<Navigate to="/" replace />} />
+                <Route path="*" element={<Navigate to="/login" replace />} />
             </Routes>
-            <Footer/>
+            {/*<Footer/>*/}
         </div>
     )
 }
